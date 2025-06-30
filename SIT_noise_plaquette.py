d1f5281c43cbce7a13efc8049c8eec1a77ab88f9
Jonathan Curtis <jon.curtis.94@gmail.com>
2025-06-30 15:21:19 +0200

    Continued testing of both mean field and single plaquette ansatze

--- SIT_noise_plaquette.py
+++ SIT_noise_plaquette.py
@@ -12,7 +12,6 @@ from matplotlib import cm
 from matplotlib import colors as mclr
 
 rng = np.random.default_rng()
-dim = 3**4
 
 ### we use matrices [0,1,2,3] to correspond to [identity, paul_x,pauli_y,pauli_z]
 spin_one_matrices = [ np.array([[1.,0.,0.],[0.,1.,0.],[0.,0.,1.]],dtype=complex), 1./np.sqrt(2.)*np.array([[0.,1.,0.],[1.,0.,1.],[0.,1.,0.]],dtype=complex),1./np.sqrt(2.)*np.array([[0.,-1.j,0.],[1.j,0.,-1.j],[0.,1.j,0.]],dtype=complex),np.array([[1.,0.,0.],[0.,0.,0.],[0.,0.,-1.]],dtype=complex) ]
@@ -25,24 +24,57 @@ S = [ [ np.kron(spin_one_matrices[0],np.kron(spin_one_matrices[0],np.kron(spin_o
 [ np.kron(spin_one_matrices[0],np.kron(spin_one_matrices[c],np.kron(spin_one_matrices[0],spin_one_matrices[0]) ) ) for c in range(4) ] ,
 [ np.kron(spin_one_matrices[c],np.kron(spin_one_matrices[0],np.kron(spin_one_matrices[0],spin_one_matrices[0]) ) ) for c in range(4) ] ]
 
-def H(Ec,Ej,lp):
+
+dim = (S[0][0]).shape[0] ### Judge from the size of the matrix which should be 81
+
+
+### This is the hamiltonian for a plaquette with a flux phi threaded 
+def H(Ec,Ej,flux):
 	Hc = Ec*sum([ S[r][3]@S[r][3] for r in range(4) ])
-	Hj = -Ej*sum([ sum([ S[r-1][c]@S[r][c] for c in [1,2] ]) for r in range(4) ])
 
-	Vp = -0.25*lp*sum([ S[r][2]@S[r-1][1]  - S[r][1]@S[r-1][2]  for r in range(4) ])
+	hopping_phase = np.exp(-1.j*flux)
+	### Hopping out of the page 
+	Hccw = -0.5*Ej*hopping_phase*sum([ (S[r][1]+1.j*S[r][2])@(S[r-1][1]-1.j*S[r-1][2]) for r in range(4) ])
+	Hcw = -0.5*Ej*np.conjugate(hopping_phase)*sum([ (S[r][1]-1.j*S[r][2])@(S[r-1][1]+1.j*S[r-1][2]) for r in range(4) ])
 
-	return Hc + Hj + Vp
+
+	return Hc + Hccw + Hcw
 
 def find_GS(Ec,Ej):
 	h = H(Ec,Ej,0.)
 
 	es,psis = np.linalg.eigh(h)
 
-	return es[0],psis[0,:]
+	return es[0],psis[:,0]
 
 ### Evolution function under Hamiltonian h 
 def eom_h(t,X,h):
 	return -1.j*h@X
+
+
+### This method evolves from the ground state under a Hamiltonian with given Ej and Ec and a time-dependent flux which switches sign at indicated times 
+def evolve_quench(Ec,Ej,echo_times,flux,sample_times):
+
+	###  First we define the quench function 
+	def quench_function(t):
+		bools = t>echo_times
+
+		if (bools==False).all() or (bools == True).all():
+			return 0. ### We are outside the echo sequence
+
+		else:
+			return flux*(-1)**int(np.sum(bools)+1)
+
+	### now we generate the Hamiltonian 
+	def eom_quench(t,X): 
+		return -1.j*H(Ec,Ej,quench_function(t))@X
+
+	### Now we find the ground state 
+	e_gs, psi_gs = find_GS(Ec,Ej)
+
+	### Next we evolve under the equations of motion 
+	sol = intg.solve_ivp(eom_quench,(sample_times[0],sample_times[-1]),psi_gs,t_eval=sample_times)
+	return sol.y
 
 
 def Ramsey_echo(tRs,lps,Ec,Ej):
@@ -52,6 +84,7 @@ def Ramsey_echo(tRs,lps,Ec,Ej):
 	e_gs, psi_gs = find_GS(Ec,Ej)
 
 	echos = np.zeros((nlp,ntR),dtype=complex)
+	psi_t = np.zeros((dim,ntR),dtype=complex)
 
 	for i in range(nlp):
 		h = H(Ec,Ej,lps[i])
@@ -61,7 +94,7 @@ def Ramsey_echo(tRs,lps,Ec,Ej):
 		for j in range(ntR):
 			echos[i,j] = np.conjugate(psi_gs)@(psi_t[:,j])
 
-	return echos
+	return psi_t
 
 ### Double check this can be written as overlap of two different Ramsey echos 
 ### Seems like this is missing an extra period of evolution 
@@ -83,7 +116,8 @@ def Hahn_echo(tHs,lps,Ec,Ej):
 		for j in range(ntH):
 			echos[i,j] = np.conjugate(psi_2[:,j])@(psi_1[:,j])
 
-	return echos
+	return psi_1, psi_2
+	#return echos
 
 
 
